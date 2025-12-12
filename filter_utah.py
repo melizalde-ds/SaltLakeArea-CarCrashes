5446c5b0b5ff1983873d3febc2b35be61e73c4dc
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2025-12-12 08:32:20 -0700

    latest

--- filter_utah.py
+++ filter_utah.py
@@ -13,18 +13,31 @@ def filter_salt_lake():
     salt_lake.to_csv('Data/Salt_Lake.csv', index=False)
 
 
+def filter_fuzzy_I15():
+    salt_lake = pd.read_csv('Data/Salt_Lake.csv')
+    salt_lake.dropna(subset=['Street'], inplace=True)
+    salt_lake['Street'] = salt_lake['Street'].str.upper()
+    fuzzy = salt_lake[salt_lake['Street'].str.contains('15')]
+    fuzzy = fuzzy[fuzzy['Street'].str.contains('I')]
+    fuzzy = fuzzy[~fuzzy['Street'].str.contains('215')]
+    fuzzy.to_csv('Data/I15.csv', index=False)
+
+
 def menu():
     loop = True
     while (loop):
         print('1. Filter Utah')
         print('2. Filter Salt Lake')
-        print('3. Exit')
+        print('3. Filter Fuzzy I15')
+        print('4. Exit')
         choice = int(input('Enter choice: '))
         if choice == 1:
             filter_utah()
         elif choice == 2:
             filter_salt_lake()
         elif choice == 3:
+            filter_fuzzy_I15()
+        elif choice == 4:
             loop = False
         else:
             print('Invalid choice')
